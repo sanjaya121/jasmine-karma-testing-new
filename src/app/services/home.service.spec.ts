449a92353615464246ab02e48a44366a2787121d
Sanjaya Chaudhary <sanjaya121@gmail.com>
2024-10-01 13:10:54 -0500

    http service Testing

--- src/app/services/home.service.spec.ts
+++ src/app/services/home.service.spec.ts
@@ -1,16 +1,39 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HomeService } from './home.service';
 
 describe('HomeService', () => {
   let service: HomeService;
+  let httpTestingController : HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
+      providers:[HomeService]
+    });
     service = TestBed.inject(HomeService);
+    httpTestingController=TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    // Ensure that there are no outstanding requests
+    // httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+    
   });
+
+  it('should get Data from Api',()=>{
+    const mockData = {
+      "userId": 1,
+      "id": 2,
+      "title": "qui est esse",
+      "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+      };
+    service.getData().subscribe(data=>{
+      expect(data).toEqual(mockData);
+    })
+  })
 });
--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,10 @@ import { Injectable } from '@angular/core';
 })
 export class HomeService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+
+  getData=()=>{
+return this.http.get('https://jsonplaceholder.typicode.com/posts');
+  }
 }
